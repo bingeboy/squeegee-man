43223e2d715d1a7de8cf8236ad691935d46d074d
Erwan Jegouzo <erwan.jegouzo@gmail.com>
2013-07-09 14:04:42 -0400

    nailed prompt

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 var prompt = require('prompt');
 
 
+// process.argv.forEach(function(val, index, array) {
+//   console.log(index + ': ' + val);
+// });
+
+
  var schema = {
     properties: {
-      transit: {
-        description: "How do you want to get there (walk, drive, transit)?",
-        pattern: /(walk|drive|transit)$/,
-        message: 'Must be one of those options',
-        required: true
-      },
       origin: {
         description: "Where do you want to start?",
         required: true
       },
       destination: {
         description: "Where Are you going?",
+        required: true
+      },
+      transit: {
+        description: "How do you want to get there (walk, drive, transit)?",
+        pattern: /(walk|drive|transit)$/,
+        message: 'Must be one of those options',
         required: true
       }
     }
@@ -29,10 +34,8 @@ var prompt = require('prompt');
   // Get two properties from the user: email, password
   //
   prompt.get(schema, function (err, result) {
-    //
-    // Log the results.
-    //
-    console.log('Command-line input received:');
-    console.log('  name: ' + result.name);
-    console.log('  password: ' + result.password);
+
+    var url = "http://maps.googleapis.com/maps/api/directions/json?"+
+              "origin="+result.origin+"&destination="+result.destination+"&sensor=false&departure_time=1343605500&mode="+result.transit;
+    console.log("url", url);
   });
